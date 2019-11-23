71bdff30cf56025313d4e21c3d2e9759b4292a2d
vKobylchak <vkobylchak@corevalue.net>
2019-11-23 20:07:10 +0200

    update CommentDao mostActiveCommenters

--- src/main/java/mflix/api/daos/CommentDao.java
+++ src/main/java/mflix/api/daos/CommentDao.java
@@ -1,16 +1,10 @@
 package mflix.api.daos;
 
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoWriteException;
-import com.mongodb.ReadConcern;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Sorts;
-import com.mongodb.client.model.Updates;
-import com.mongodb.client.result.DeleteResult;
-import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.MongoCursor;
 import mflix.api.models.Comment;
 import mflix.api.models.Critic;
 import org.bson.Document;
@@ -24,16 +18,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
+import static com.mongodb.client.model.Accumulators.first;
+import static com.mongodb.client.model.Accumulators.sum;
+import static com.mongodb.client.model.Aggregates.*;
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Sorts.descending;
 import static com.mongodb.client.model.Updates.set;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 
 @Component
 public class CommentDao extends AbstractMFlixDao {
@@ -41,6 +37,7 @@ public class CommentDao extends AbstractMFlixDao {
     public static String COMMENT_COLLECTION = "comments";
 
     private MongoCollection<Comment> commentCollection;
+    private MongoCollection<Document> documentCommentCollection;
 
     private CodecRegistry pojoCodecRegistry;
 
@@ -58,6 +55,7 @@ public class CommentDao extends AbstractMFlixDao {
                         fromProviders(PojoCodecProvider.builder().automatic(true).build()));
         this.commentCollection =
                 db.getCollection(COMMENT_COLLECTION, Comment.class).withCodecRegistry(pojoCodecRegistry);
+        this.documentCommentCollection = db.getCollection(COMMENT_COLLECTION);
     }
 
     /**
@@ -82,7 +80,7 @@ public class CommentDao extends AbstractMFlixDao {
      * returns the resulting Comment object.
      */
     public Comment addComment(Comment comment) {
-      if (null == comment.getId()) throw new IncorrectDaoOperation("FAKE ID");
+        if (null == comment.getId()) throw new IncorrectDaoOperation("FAKE ID");
 //        Comment actualComment = commentCollection.find(new Document("_id", new ObjectId(comment.getId()))).first();
 //        try {
 //            if (actualComment == null) throw new IncorrectDaoOperation("Given a wrong commentId.");
@@ -90,7 +88,7 @@ public class CommentDao extends AbstractMFlixDao {
 //            e.printStackTrace();
 //            return null;
 //        }
-          commentCollection.insertOne(comment);
+        commentCollection.insertOne(comment);
         // TODO> Ticket - Update User reviews: implement the functionality that enables adding a new
         // comment.
         // TODO> Ticket - Handling Errors: Implement a try catch block to
@@ -121,14 +119,14 @@ public class CommentDao extends AbstractMFlixDao {
     public boolean updateComment(String commentId, String text, String email) {
 //      Bson match = eq("_id",
 //              new ObjectId(commentId));
-      Bson match =and(eq("_id",
-              new ObjectId(commentId)), eq("email", email));
+        Bson match = and(eq("_id",
+                new ObjectId(commentId)), eq("email", email));
 //      Bson set = set("text", text);
-      Bson set = and(set("text", text), set("date", new Date()));
-      Comment comment = commentCollection.find(match).first();
-      if (null == comment) return false;
+        Bson set = and(set("text", text), set("date", new Date()));
+        Comment comment = commentCollection.find(match).first();
+        if (null == comment) return false;
 
-      commentCollection.updateOne(match, set);
+        commentCollection.updateOne(match, set);
         // TODO> Ticket - Update User reviews: implement the functionality that enables updating an
         // user own comments
         // TODO> Ticket - Handling Errors: Implement a try catch block to
@@ -144,12 +142,18 @@ public class CommentDao extends AbstractMFlixDao {
      * @return true if successful deletes the comment.
      */
     public boolean deleteComment(String commentId, String email) {
+        Bson match = and(eq("_id",
+                new ObjectId(commentId)), eq("email", email));
+        Comment comment = commentCollection.find(match).first();
+        if (null == comment) return false;
+        commentCollection.deleteOne(match);
+
         // TODO> Ticket Delete Comments - Implement the method that enables the deletion of a user
         // comment
         // TIP: make sure to match only users that own the given commentId
         // TODO> Ticket Handling Errors - Implement a try catch block to
         // handle a potential write exception when given a wrong commentId.
-        return false;
+        return true;
     }
 
     /**
@@ -161,12 +165,38 @@ public class CommentDao extends AbstractMFlixDao {
      */
     public List<Critic> mostActiveCommenters() {
         List<Critic> mostActive = new ArrayList<>();
+        List<Document> mostActiveDocument = new ArrayList<>();
+
+        Bson match = eq("email", "andrea_le@fakegmail.com");
+        long count = commentCollection.countDocuments(match);
+        System.out.println(count);
+
+//        List<Bson> bsonComments = Arrays.asList(group("$email", sum("comments", 1L)), sort(descending("comments")), limit(20));
+
+        List<Bson> bsonComments = Arrays.asList(group("$email", first("name", "$name"), first("email", "$email"), first("movie_id", "$movie_id"), first("text", "$text"), first("date", "$date"), sum("comments", 1L)), sort(descending("comments")), limit(20));
         // // TODO> Ticket: User Report - execute a command that returns the
         // // list of 20 users, group by number of comments. Don't forget,
         // // this report is expected to be produced with an high durability
         // // guarantee for the returned documents. Once a commenter is in the
         // // top 20 of users, they become a Critic, so mostActive is composed of
         // // Critic objects.
+
+
+        AggregateIterable<Document> aggregate = documentCommentCollection.aggregate(bsonComments);
+//        AggregateIterable<Comment> aggregate1 = commentCollection.aggregate(bsonComments);
+
+        aggregate.iterator().forEachRemaining(mostActiveDocument::add);
+
+        mostActiveDocument.forEach(document -> {
+            Critic critic = new Critic();
+            String email = document.getString("email");
+            long commentsLong = (Long) document.get("comments");
+            int numComments = (int)commentsLong;
+            critic.setId(email);
+            critic.setNumComments(numComments);
+            mostActive.add(critic);
+        });
+
         return mostActive;
     }
 }
